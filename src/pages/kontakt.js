03e7110c9ae840f6a7fee58a46f5b5d31eafa9e5
Marek Melichar <koprivackej@gmail.com>
2017-11-13 22:28:31 +0100

    13_11_2017

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -2,11 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const ContactPage = () => (
-  <div>
-    <h1>Hi from the contact page</h1>
-    <p>Welcome to contact page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+  <section>
+    <div className="row contact-page">
+			<div className="column size_100">
+					<form>
+						<div style="display: none;"> <input type="hidden" name="_wpcf7" value="70" /> <input type="hidden" name="_wpcf7_version" value="4.9.1" /> <input type="hidden" name="_wpcf7_locale" value="en_US" /> <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f70-p19-o1" /> <input
+							  type="hidden" name="_wpcf7_container_post" value="19" /></div>
+						<p><span className="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" id="contactEmail" aria-required="true" aria-invalid="false" placeholder="E-mail" /></span><br
+            /> <span className="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" id="contactName" aria-required="true" aria-invalid="false" placeholder="Jméno" /></span><br
+            /> <span className="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" className="wpcf7-form-control wpcf7-text" id="contactSubject" aria-invalid="false" placeholder="Předmět" /></span><br /> <span className="wpcf7-form-control-wrap textarea"><textarea name="textarea" cols="40" rows="10" className="wpcf7-form-control wpcf7-textarea" id="contactText" aria-invalid="false" placeholder="Vzkaz"></textarea></span></p>
+						<p
+						  className="submit-icon"><span className="ion-ios-paperplane-outline"></span><input type="submit" value="Send" className="wpcf7-form-control wpcf7-submit" id="sipkaOdeslat" /></p>
+							<div className="wpcf7-response-output wpcf7-display-none"></div>
+					</form>
+			</div>
+		</div>
+		<div className="map"></div>
+  </section>
 )
 
 export default ContactPage
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Portfolio1 from '../img/portfolio/1.jpg'
+import Portfolio2 from '../img/portfolio/2.jpg'
+import Portfolio3 from '../img/portfolio/3.jpg'
+import Portfolio4 from '../img/portfolio/4.jpg'
+import Portfolio5 from '../img/portfolio/5.jpg'
+import Portfolio6 from '../img/portfolio/6.jpg'
+
 const PortfolioPage = () => (
-  <div>
-    <h1>Hi from the portfolio page</h1>
-    <p>Welcome to page portfolio</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+  <section className="portfolio">
+    <div class="row">
+			<div class="column size_100">
+				<h3>PORTFOLIO</h3>
+			</div>
+		</div>
+    <div className="image-grid group">
+      <img src={Portfolio1} className="gallery-img" alt="Výškové práce Melichar" />
+      <img src={Portfolio2} className="gallery-img" alt="Lesní mateřská škola Permoník" />
+      <img src={Portfolio3} className="gallery-img" alt="Bottle service" />
+      <img src={Portfolio4} className="gallery-img" alt="Kovářství Uhříček" />
+      <img src={Portfolio5} className="gallery-img" alt="epoznamky.cz" />
+      <img src={Portfolio6} className="gallery-img" alt="LanCraft" />
+    </div>
+  </section>
 )
 
 export default PortfolioPage
